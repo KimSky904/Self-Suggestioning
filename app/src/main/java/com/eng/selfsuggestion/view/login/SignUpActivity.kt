f66756edb0c54712edc5f080efd4ea56ffc43819
KimHaNeul <s2025@e-mirim.hs.kr>
2022-08-10 21:08:17 +0100

    init

---
+++ app/src/main/java/com/eng/selfsuggestion/view/login/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package com.eng.selfsuggestion.view.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.eng.selfsuggestion.R
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
