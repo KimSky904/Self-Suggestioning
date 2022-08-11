146943ec9a91524c22a7a44308fbd5367fdecc01
KimHaNeul <s2025@e-mirim.hs.kr>
2022-08-11 13:52:05 +0100

    (#3) viewbinding 적용
    - bottom navigation bar

--- app/src/main/java/com/eng/selfsuggestion/MainActivity.kt
+++ app/src/main/java/com/eng/selfsuggestion/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.eng.selfsuggestion
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.eng.selfsuggestion.databinding.ActivityMainBinding
+import com.eng.selfsuggestion.view.navigation.BaseActivity
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnSignIn.setOnClickListener {
+            val intent = Intent(this,BaseActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
---
+++ app/src/main/java/com/eng/selfsuggestion/view/navigation/BaseActivity.kt
@@ -0,0 +1,48 @@
+package com.eng.selfsuggestion.view.navigation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import com.eng.selfsuggestion.R
+import com.eng.selfsuggestion.databinding.ActivityBaseBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class BaseActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityBaseBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityBaseBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        replaceFragment(HomeFragment())
+        binding.bottomNavigation.setOnItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.action_list -> {
+                    Log.e("TAG","List")
+                    replaceFragment(ListFragment())
+                    true
+                }
+                R.id.action_home -> {
+                    Log.e("TAG","Home")
+                    replaceFragment(HomeFragment())
+                    true
+                }
+                R.id.action_settings -> {
+                    Log.e("TAG","Settings")
+                    replaceFragment(SettingFragment())
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+    private fun replaceFragment(fragment : Fragment) {
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.layout_frame,fragment)
+        fragmentTransaction.commit()
+    }
+}
