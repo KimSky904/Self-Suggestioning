feca63a417d7b671998212ba46bde154066ac7ed
KimHaNeul <s2025@e-mirim.hs.kr>
2022-08-11 23:02:57 +0100

    background design 적용
    - home fragment
    - list fragment viewpager (not completed)

--- app/src/main/java/com/eng/selfsuggestion/view/navigation/ListFragment.kt
+++ app/src/main/java/com/eng/selfsuggestion/view/navigation/ListFragment.kt
@@ -1,60 +1,92 @@
 package com.eng.selfsuggestion.view.navigation
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.eng.selfsuggestion.R
+import android.widget.FrameLayout
+import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import androidx.viewpager.widget.PagerAdapter
+import androidx.viewpager.widget.ViewPager
+import androidx.viewpager.widget.ViewPager.OnPageChangeListener
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.eng.selfsuggestion.databinding.FragmentListBinding
+import com.eng.selfsuggestion.view.spell.DailySpellFragment
+import com.eng.selfsuggestion.view.spell.SpecialSpellFragment
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ListFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ListFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private lateinit var _binding : FragmentListBinding
+    private var indicatorWidth : Int = 0
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false)
+        _binding = FragmentListBinding.inflate(inflater, container, false)
+
+        val adapter = TabFragmentAdapter(requireFragmentManager())
+        _binding.viewPager.adapter = adapter
+        _binding.tabLayout.setupWithViewPager(_binding.viewPager)
+
+        _binding.tabLayout.post {
+            indicatorWidth = _binding.tabLayout.width / _binding.tabLayout.tabCount
+
+            val indicatorParams: FrameLayout.LayoutParams =
+                _binding.indicator.layoutParams as FrameLayout.LayoutParams
+            indicatorParams.width = indicatorWidth
+            _binding.indicator.layoutParams = indicatorParams
+        }
+
+        _binding.viewPager.addOnPageChangeListener(object : OnPageChangeListener {
+            override fun onPageScrolled(
+                position: Int,
+                positionOffset: Float,
+                positionOffsetPixels: Int
+            ) {
+                val params : FrameLayout.LayoutParams = _binding.indicator.layoutParams as FrameLayout.LayoutParams
+                val translationOffset = (position + positionOffset) * indicatorWidth
+                params.leftMargin = translationOffset.toInt()
+                _binding.indicator.layoutParams = params
+            }
+
+            override fun onPageSelected(position: Int) {}
+            override fun onPageScrollStateChanged(state: Int) {}
+
+        })
+
+
+        return _binding.root
+    }
+}
+
+private class TabFragmentAdapter (
+    fragmentManager : FragmentManager
+) : FragmentPagerAdapter(fragmentManager) {
+
+    private var arrayList : ArrayList<Fragment> = ArrayList()
+    init {
+        arrayList.add(DailySpellFragment())
+        arrayList.add(SpecialSpellFragment())
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ListFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ListFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun getCount(): Int {
+        return arrayList.size
     }
+
+    override fun getItem(position: Int): Fragment {
+        return arrayList[position]
+    }
+
 }
