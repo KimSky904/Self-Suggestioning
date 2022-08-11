ab9c99e8c67648f93d2485639653f1aecf99dd9c
KimHaNeul <s2025@e-mirim.hs.kr>
2022-08-12 00:43:46 +0100

    expandable action button

--- app/src/main/java/com/eng/selfsuggestion/view/navigation/ListFragment.kt
+++ app/src/main/java/com/eng/selfsuggestion/view/navigation/ListFragment.kt
@@ -1,27 +1,30 @@
 package com.eng.selfsuggestion.view.navigation
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.FrameLayout
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import androidx.fragment.app.FragmentActivity
-import androidx.viewpager.widget.PagerAdapter
-import androidx.viewpager.widget.ViewPager
-import androidx.viewpager.widget.ViewPager.OnPageChangeListener
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.eng.selfsuggestion.R
 import com.eng.selfsuggestion.databinding.FragmentListBinding
 import com.eng.selfsuggestion.view.spell.DailySpellFragment
 import com.eng.selfsuggestion.view.spell.SpecialSpellFragment
 import com.google.android.material.tabs.TabLayout
-import com.google.android.material.tabs.TabLayoutMediator
 
 class ListFragment : Fragment() {
 
     private lateinit var _binding : FragmentListBinding
-    private var indicatorWidth : Int = 0
+    // private var indicatorWidth : Int = 0
+    private var isClicked : Boolean = false
+    private val rotateOpen : Animation by lazy { AnimationUtils.loadAnimation(context, R.anim.rotate_open_anim) }
+    private val rotateClose : Animation by lazy { AnimationUtils.loadAnimation(context, R.anim.rotate_close_anim) }
+    private val fromBottom : Animation by lazy { AnimationUtils.loadAnimation(context, R.anim.from_bottom_anim) }
+    private val toBottom : Animation by lazy { AnimationUtils.loadAnimation(context, R.anim.to_bottom_anim) }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,7 +46,65 @@ class ListFragment : Fragment() {
             }
         })
 
+        _binding.btnFloatingAction.setOnClickListener {
+            onAddButtonClicked()
+        }
+        _binding.btnToOthers.setOnClickListener {
+
+        }
+        _binding.btnDailySpell.setOnClickListener {
+
+        }
+        _binding.btnSpecialSpell.setOnClickListener {
+
+        }
+
+
         return _binding.root
+    }
+
+    private fun onAddButtonClicked() {
+        setVisibility(isClicked)
+        setAnimation(isClicked)
+        isClicked = !isClicked
+    }
+    private fun setVisibility(isClicked : Boolean) {
+        if(!isClicked) {
+            _binding.btnToOthers.visibility = View.VISIBLE
+            _binding.btnDailySpell.visibility = View.VISIBLE
+            _binding.btnSpecialSpell.visibility = View.VISIBLE
+
+            _binding.txtToOthers.visibility = View.VISIBLE
+            _binding.txtDailySpell.visibility = View.VISIBLE
+            _binding.txtSpecialSpell.visibility = View.VISIBLE
+        } else {
+            _binding.btnToOthers.visibility = View.INVISIBLE
+            _binding.btnDailySpell.visibility = View.INVISIBLE
+            _binding.btnSpecialSpell.visibility = View.INVISIBLE
+
+            _binding.txtToOthers.visibility = View.INVISIBLE
+            _binding.txtDailySpell.visibility = View.INVISIBLE
+            _binding.txtSpecialSpell.visibility = View.INVISIBLE
+        }
+    }
+    private fun setAnimation(isClicked: Boolean) {
+        if(!isClicked) {
+            _binding.btnToOthers.startAnimation(fromBottom)
+            _binding.btnDailySpell.startAnimation(fromBottom)
+            _binding.btnSpecialSpell.startAnimation(fromBottom)
+            _binding.txtToOthers.startAnimation(fromBottom)
+            _binding.txtDailySpell.startAnimation(fromBottom)
+            _binding.txtSpecialSpell.startAnimation(fromBottom)
+            _binding.btnFloatingAction.startAnimation(rotateOpen)
+        } else {
+            _binding.btnToOthers.startAnimation(toBottom)
+            _binding.btnDailySpell.startAnimation(toBottom)
+            _binding.btnSpecialSpell.startAnimation(toBottom)
+            _binding.txtToOthers.startAnimation(toBottom)
+            _binding.txtDailySpell.startAnimation(toBottom)
+            _binding.txtSpecialSpell.startAnimation(toBottom)
+            _binding.btnFloatingAction.startAnimation(rotateClose)
+        }
     }
 }
 
