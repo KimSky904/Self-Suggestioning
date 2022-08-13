ddce8513ce914e29a82670c67b7431cf7a4c498c
Jyuri <jeon1310@gmail.com>
2022-08-14 06:44:57 +0900

    add the auth in MainActivity.kt

--- app/src/main/java/com/eng/selfsuggestion/MainActivity.kt
+++ app/src/main/java/com/eng/selfsuggestion/MainActivity.kt
@@ -1,24 +1,59 @@
 package com.eng.selfsuggestion
 
+import android.content.ContentValues
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.eng.selfsuggestion.databinding.ActivityMainBinding
 import com.eng.selfsuggestion.view.navigation.BaseActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding : ActivityMainBinding
+    private lateinit var auth : FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        auth = Firebase.auth
 
         binding.btnSignIn.setOnClickListener {
             val intent = Intent(this,BaseActivity::class.java)
             startActivity(intent)
         }
 
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        if (auth.currentUser == null){ // check user
+            // haven't auth
+            signInAnonymously()
+        }
+        Log.i(ContentValues.TAG, "onStart: currentUser"+auth.uid)
+    }
+
+    // create user anonymously
+    fun signInAnonymously(){
+        auth.signInAnonymously()
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(ContentValues.TAG, "signInAnonymously:success")
+                    val current = auth.currentUser
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(ContentValues.TAG, "signInAnonymously:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
