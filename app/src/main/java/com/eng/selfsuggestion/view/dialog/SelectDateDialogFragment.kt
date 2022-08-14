122eb2cd057a5d6dff6aaf1722dfb73a674d1a14
KimHaNeul <s2025@e-mirim.hs.kr>
2022-08-15 00:17:30 +0100

    ready for special spell connecting with firebase
    - add delete, edit dialog

--- app/src/main/java/com/eng/selfsuggestion/view/dialog/SelectDateDialogFragment.kt
+++ app/src/main/java/com/eng/selfsuggestion/view/dialog/SelectDateDialogFragment.kt
@@ -3,23 +3,17 @@ package com.eng.selfsuggestion.view.dialog
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.NumberPicker
 import androidx.fragment.app.DialogFragment
-import com.eng.selfsuggestion.R
 import com.eng.selfsuggestion.databinding.FragmentSelectDateDialogBinding
-import com.eng.selfsuggestion.model.SendingModel
-import java.lang.IllegalArgumentException
 import java.util.*
-import kotlin.math.max
 
 class SelectDateDialogFragment : DialogFragment() {
 
-    private lateinit var _binding : FragmentSelectDateDialogBinding
+    private lateinit var _binding: FragmentSelectDateDialogBinding
+    private lateinit var listener : DialogSaveClickedListener
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,66 +22,47 @@ class SelectDateDialogFragment : DialogFragment() {
         _binding = FragmentSelectDateDialogBinding.inflate(inflater, container, false)
         dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
 
-//        val monthValuesList : List<String> = listOf("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")
-//        setNumberPicker(_binding.pickerMonth, monthValuesList)
-//
-//        val yearValuesList : MutableList<String> = mutableListOf()
-//        for(i in Calendar.YEAR..Calendar.YEAR + 5) {
-//            yearValuesList += i.toString()
-//        }
-//        setNumberPicker(_binding.pickerYear, yearValuesList)
-//
-//        val max = getMaxDateInMonth(Calendar.MONTH, Calendar.YEAR)
-//        val dateValuesList : MutableList<String> = mutableListOf()
-//        for(i in 1..max) {
-//            dateValuesList += i.toString()
-//        }
-//        setNumberPicker(_binding.pickerDate, dateValuesList)
-
-
         _binding.btnSave.setOnClickListener {
-            _binding.pickerDate.year
+            val value = Date(_binding.pickerDate.year,_binding.pickerDate.month,_binding.pickerDate.dayOfMonth)
+            var strValue = when(_binding.pickerDate.month) {
+                1 -> "January"
+                2 -> "February"
+                3 -> "March"
+                4 -> "April"
+                5 -> "May"
+                6 -> "June"
+                7 -> "July"
+                8 -> "August"
+                9 -> "September"
+                10 -> "October"
+                11 -> "November"
+                12 -> "December"
+                else -> " "
+            }
+            strValue += _binding.pickerDate.dayOfMonth
+            strValue += ", " + _binding.pickerDate.year
+            listener.onSaveClicked(value, strValue)
             dialog?.dismiss()
         }
+
         _binding.btnCancel.setOnClickListener {
             dialog?.dismiss()
         }
 
-//        _binding.pickerMonth.setOnValueChangedListener { _, _, newVal ->
-//            val maxValue = getMaxDateInMonth(newVal, _binding.pickerYear.value)
-//            if(_binding.pickerDate.value > maxValue) {
-//                _binding.pickerDate.displayedValues = null
-//                _binding.pickerDate.maxValue = maxValue
-//            }
-//            fragmentManager?.beginTransaction()?.detach(this)?.attach(this)?.commit()
-//        }
-//        _binding.pickerYear.setOnValueChangedListener { _, _, newVal ->
-//            val maxValue = getMaxDateInMonth(_binding.pickerMonth.value, newVal)
-//            if(_binding.pickerDate.value > maxValue) {
-//                _binding.pickerDate.displayedValues = null
-//                _binding.pickerDate.maxValue = maxValue
-//            }
-//            fragmentManager?.beginTransaction()?.detach(this)?.attach(this)?.commit()
-//        }
-
-
 
         return _binding.root
     }
-//    private fun getMaxDateInMonth(month : Int, year : Int) : Int {
-//        return when(month-1) {
-//            Calendar.JANUARY, Calendar.MARCH, Calendar.MAY, Calendar.JULY, Calendar.AUGUST, Calendar.OCTOBER, Calendar.DECEMBER -> 31
-//            Calendar.APRIL, Calendar.JUNE, Calendar.SEPTEMBER, Calendar.NOVEMBER -> 30
-//            Calendar.FEBRUARY -> if(year%4 == 0 && year % 100 != 0 || year % 400 == 0) 29 else 28
-//            else -> throw IllegalArgumentException("Month is Wrong")
-//        }
-//    }
-//
-//    private fun setNumberPicker(numberPicker : NumberPicker, valuesList: List<String>) {
-//        numberPicker.maxValue = valuesList.size
-//        numberPicker.minValue = 1
-//        numberPicker.showDividers = NumberPicker.SHOW_DIVIDER_NONE
-//        numberPicker.wrapSelectorWheel = true
-//        numberPicker.displayedValues = valuesList.toTypedArray()
-//    }
+
+
+    fun setOnClickListener(listener: (Date, String) -> Unit) {
+        this.listener = object : DialogSaveClickedListener {
+            override fun onSaveClicked(content: Date, textValue: String) {
+                listener(content, textValue)
+            }
+        }
+    }
+
+    interface DialogSaveClickedListener {
+        fun onSaveClicked(content : Date, textValue : String)
+    }
 }
