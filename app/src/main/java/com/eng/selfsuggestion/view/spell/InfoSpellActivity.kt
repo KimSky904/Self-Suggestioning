d8f1704d875274d7b2222516db68a58bc53a2fd0
Jyuri <jeon1310@gmail.com>
2022-08-15 03:42:45 +0900

    add contextMenu logic

--- app/src/main/java/com/eng/selfsuggestion/view/spell/InfoSpellActivity.kt
+++ app/src/main/java/com/eng/selfsuggestion/view/spell/InfoSpellActivity.kt
@@ -2,11 +2,53 @@ package com.eng.selfsuggestion.view.spell
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ContextMenu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
 import com.eng.selfsuggestion.R
+import com.eng.selfsuggestion.databinding.ActivityBaseBinding
+import com.eng.selfsuggestion.databinding.ActivityInfoSpellBinding
 
 class InfoSpellActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityInfoSpellBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_info_spell)
+        binding = ActivityInfoSpellBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        registerForContextMenu(binding.infospellMenuIcon)
+    }
+
+    override fun onCreateContextMenu(
+        menu: ContextMenu?,
+        v: View?,
+        menuInfo: ContextMenu.ContextMenuInfo?
+    ) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+
+        // menu file to object
+        val inflater = menuInflater
+        inflater.inflate(R.menu.top_infospell_menu, menu)
+    }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        // menu id 식별하여 이벤트 걸기
+        when(item.itemId){
+            R.id.menu_edit -> {
+
+            }
+            R.id.menu_delete -> {
+
+            }
+        }
+        return super.onContextItemSelected(item)
+    }
+    
+    // 그냥 클릭했을 시 안내메시지 보여주기
+    fun clickToast(view : View){
+        Toast.makeText(this, "it used long click",Toast.LENGTH_SHORT).show()
     }
 }
