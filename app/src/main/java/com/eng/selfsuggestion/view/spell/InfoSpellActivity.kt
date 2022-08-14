887472401357a035487b1bdb29528c5fdb275ae3
Jyuri <jeon1310@gmail.com>
2022-08-15 02:52:08 +0900

    complated bottom_sheets.xml

---
+++ app/src/main/java/com/eng/selfsuggestion/view/spell/InfoSpellActivity.kt
@@ -0,0 +1,12 @@
+package com.eng.selfsuggestion.view.spell
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.eng.selfsuggestion.R
+
+class InfoSpellActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_info_spell)
+    }
+}
