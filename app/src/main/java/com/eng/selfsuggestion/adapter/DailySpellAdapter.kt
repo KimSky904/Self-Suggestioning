580ed9d687a5433703e971496e6414d8a719c9da
Jyuri <jeon1310@gmail.com>
2022-08-15 09:38:13 +0900

    create specialSpell

--- app/src/main/java/com/eng/selfsuggestion/adapter/DailySpellAdapter.kt
+++ app/src/main/java/com/eng/selfsuggestion/adapter/DailySpellAdapter.kt
@@ -1,14 +1,20 @@
 package com.eng.selfsuggestion.adapter
 
+import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.fragment.app.FragmentManager
 import androidx.recyclerview.widget.RecyclerView
 import com.eng.selfsuggestion.R
 import com.eng.selfsuggestion.model.RoutineModel
+import com.eng.selfsuggestion.view.dialog.SpellListFunctionDialogFragment
+import com.eng.selfsuggestion.view.spell.InfoSpellActivity
 
-class DailySpellAdapter() : RecyclerView.Adapter<DailySpellAdapter.ViewHolder>() {
+class DailySpellAdapter(private val context : Context?, private val fragmentManager: FragmentManager,) : RecyclerView.Adapter<DailySpellAdapter.ViewHolder>() {
 
     private var dailyList: ArrayList<RoutineModel> = ArrayList()
 
@@ -17,10 +23,16 @@ class DailySpellAdapter() : RecyclerView.Adapter<DailySpellAdapter.ViewHolder>()
 
         private val titleText : TextView = itemView.findViewById(R.id.txt_item_title)
         private val dateText : TextView = itemView.findViewById(R.id.txt_item_date)
+        private val btnMore : ImageView = itemView.findViewById(R.id.icon_more)
 
         fun bind(item: RoutineModel) {
             titleText.text = item.content
             dateText.text = item.timestamp.toString()
+
+            btnMore.setOnClickListener {
+                val dialogFragment = SpellListFunctionDialogFragment(0)
+                dialogFragment.show(fragmentManager, "SpellListFunctionDialogFragment")
+            }
         }
     }
 
@@ -31,6 +43,13 @@ class DailySpellAdapter() : RecyclerView.Adapter<DailySpellAdapter.ViewHolder>()
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.bind(dailyList[position])
+        holder.itemView.setOnClickListener {
+            val intent = Intent(context, InfoSpellActivity::class.java)
+            intent.putExtra("content", dailyList[position].content)
+            intent.putExtra("date", dailyList[position].timestamp.toString())
+            intent.putExtra("count", dailyList[position].count)
+            context!!.startActivity(intent)
+        }
     }
 
     override fun getItemCount(): Int = dailyList.size
--- app/src/main/java/com/eng/selfsuggestion/view/spell/AddSpecialSpellActivity.kt
+++ app/src/main/java/com/eng/selfsuggestion/view/spell/AddSpecialSpellActivity.kt
@@ -4,14 +4,22 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TimePicker
 import com.eng.selfsuggestion.databinding.ActivityAddSpecialSpellBinding
+import com.eng.selfsuggestion.repository.ArrivedRepository
+import com.eng.selfsuggestion.repository.RoutineRepository
 import com.eng.selfsuggestion.view.dialog.SelectDateDialogFragment
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import java.sql.Time
+import java.text.SimpleDateFormat
 import java.util.*
 
 class AddSpecialSpellActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityAddSpecialSpellBinding
-    private var timePicker : TimePicker? = null
+    private lateinit var targetdate : String
+    private lateinit var scopeIO : CoroutineScope
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,8 +30,8 @@ class AddSpecialSpellActivity : AppCompatActivity() {
             val dialogFragment = SelectDateDialogFragment()
             dialogFragment.setOnClickListener { content, textValue ->
                 binding.txtSpellDate.text = textValue
-                // TODO : content(현재 자료형 : Date) 형변환 필요
-                timePicker = content as TimePicker
+                scopeIO = CoroutineScope(Dispatchers.IO)
+                targetdate = SimpleDateFormat("yyyy/MM/dd").format(content)
             }
             dialogFragment.show(supportFragmentManager, "SelectDateDialogFragment")
         }
@@ -32,8 +40,24 @@ class AddSpecialSpellActivity : AppCompatActivity() {
             // spell_name : spell name
             // time_picker : 사용자가 입력한 Date, 위에서 형변환 필요
             val spellName = binding.edittextSpellName.text
-            if(!spellName.isBlank() && timePicker != null) {
+            if(!spellName.isBlank() && targetdate != null) {
                 // TODO : firebase에 저장~!
+                val auth = FirebaseAuth.getInstance()
+                val date = Calendar.getInstance().time
+
+                val data = mapOf<String,Any>(
+                    "contents" to binding.edittextSpellName.text.toString(),
+                    "count" to 0,
+                    "timestamp" to date,
+                    "arrivedate" to targetdate,
+                    "uid" to auth.uid.toString()
+                )
+
+                // create firebase specialspell
+                scopeIO.launch {
+                    val arriveRef = ArrivedRepository
+                    arriveRef.createArrived(data)
+                }
             }
         }
     }
