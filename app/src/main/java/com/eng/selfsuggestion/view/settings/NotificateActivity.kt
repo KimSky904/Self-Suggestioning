d4c8bb860d78b0363bbb0d32eec84d7597d07aba
Jyuri <jeon1310@gmail.com>
2022-08-16 11:06:40 +0900

    icon fetch

--- app/src/main/java/com/eng/selfsuggestion/view/settings/NotificateActivity.kt
+++ app/src/main/java/com/eng/selfsuggestion/view/settings/NotificateActivity.kt
@@ -3,8 +3,10 @@ package com.eng.selfsuggestion.view.settings
 import android.animation.ObjectAnimator
 import android.app.AlarmManager
 import android.app.PendingIntent
+import android.content.ContentValues.TAG
 import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -15,6 +17,7 @@ import android.view.animation.AnimationUtils
 import android.widget.LinearLayout
 import android.widget.NumberPicker
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.transition.Fade
 import androidx.transition.Transition
 import androidx.transition.TransitionManager
@@ -29,19 +32,32 @@ import java.util.*
 class NotificateActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityNotificateBinding
-    private var isStateOn = false
+    private val pref : SharedPreferences by lazy { getSharedPreferences("pref", MODE_PRIVATE) }
+    private var isStateOn : Boolean = true
 
     private val fromBottom : Animation by lazy { AnimationUtils.loadAnimation(baseContext, R.anim.from_bottom_anim) }
     private val toBottom : Animation by lazy { AnimationUtils.loadAnimation(baseContext, R.anim.to_bottom_anim) }
     private val defaultScope = CoroutineScope(Dispatchers.Default)
 
+    var alarmMgr: AlarmManager? = null
+    var alarmIntent: PendingIntent? = null
+    
+    // on / off 설정하기
 
+    @RequiresApi(31)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityNotificateBinding.inflate(layoutInflater)
+
         setContentView(binding.root)
         initView()
 
+        // 이전에 설정했던 시간 가져오기
+        val hour = pref.getInt("alarmhour",0)
+        val minute = pref.getInt("alarmminute",0)
+
+        binding.hourPicker.value = hour
+        binding.minutePicker.value = minute
 
         binding.txtIndicatorOff.setOnClickListener {
             if(isStateOn) {
@@ -58,6 +74,13 @@ class NotificateActivity : AppCompatActivity() {
                 }, 350)
 
                 isStateOn = !isStateOn
+            }
+            alarmCencel()
+
+            // alarm off setting
+            with(pref.edit()){
+                putBoolean("alarm",false)
+                apply()
             }
         }
         binding.txtIndicatorOn.setOnClickListener {
@@ -85,10 +108,25 @@ class NotificateActivity : AppCompatActivity() {
                 set(Calendar.MINUTE, binding.minutePicker.value)
             }
             alarmSetting(calendar)
+
+            // on / off, time 저장하기
+            with(pref.edit()){
+                putBoolean("alarm",true)
+                putInt("alarmhour",binding.hourPicker.value)
+                putInt("alarmminute",binding.minutePicker.value)
+                apply()
+            }
+            Log.i(TAG, "initView: notification save"+pref.getBoolean("alarm",false))
+
+            finish()
         }
     }
 
     private fun initView() {
+        Log.i(TAG, "initView: notification"+pref.getBoolean("alarm",false))
+        isStateOn = pref.getBoolean("alarm",false)
+
+
         binding.hourPicker.maxValue = 23
         binding.hourPicker.minValue = 0
         binding.hourPicker.wrapSelectorWheel = true
@@ -117,14 +155,12 @@ class NotificateActivity : AppCompatActivity() {
 
     // alarm setting
     // standard parameter : time set data by the user
+    @RequiresApi(31)
     fun alarmSetting(standard : Calendar){
         defaultScope.launch {
-            var alarmMgr: AlarmManager? = null
-            lateinit var alarmIntent: PendingIntent
-
             alarmMgr = this@NotificateActivity.getSystemService(Context.ALARM_SERVICE) as AlarmManager
             alarmIntent = Intent(this@NotificateActivity, UserAlarmReceiver::class.java).let { intent ->
-                PendingIntent.getBroadcast(this@NotificateActivity, 0, intent, 0)
+                PendingIntent.getBroadcast(this@NotificateActivity, 0, intent, PendingIntent.FLAG_MUTABLE)
             }
             alarmMgr?.setInexactRepeating(
                 AlarmManager.RTC_WAKEUP,
@@ -133,7 +169,11 @@ class NotificateActivity : AppCompatActivity() {
                 alarmIntent
             )
 
-            Toast.makeText(this@NotificateActivity,"Setting Alarm Time",Toast.LENGTH_SHORT).show()
         }
+        Toast.makeText(this@NotificateActivity,"Setting Alarm Time",Toast.LENGTH_SHORT).show()
+    }
+
+    fun alarmCencel(){
+        alarmMgr?.cancel(alarmIntent)
     }
 }
